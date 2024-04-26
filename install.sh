487b8c852ab432c9addf7aa4c89498b341b2c4b6
Irshad Ahmed <68029644+irshad-hub@users.noreply.github.com>
2024-04-27 03:24:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -73,7 +73,7 @@ eksctl version
 
 # kubectl
 sudo apt update
- sudo apt install curl
- curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
- sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
- kubectl version --client
+sudo apt install curl
+curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
